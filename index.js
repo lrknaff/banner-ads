4bcbf349fc3c1a4ff555b2087a03d6002f99178a
lrknaff <lacey.knaff@gmail.com>
2017-04-25 09:23:02 -0600

    collapse views

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ $('.list').on('click', 'li', function(e) {
 
   $('iframe').attr('src', link)
 });
+
+$('.inspiration').on('click', function(e) {
+  e.preventDefault();
+
+  let inspirationList = $('.inspiration-list')
+  let inspirationHeader = $('.inspiration')
+
+  if( inspirationList.attr('id') === 'hide' ) {
+    inspirationList.attr('id', 'show');
+    inspirationHeader.text('- Inspiration');
+  } else {
+    inspirationList.attr('id', 'hide');
+    inspirationHeader.text('+ Inspiration');
+  }
+});
+
+
+$('.conversion').on('click', function(e) {
+  e.preventDefault();
+
+  let conversionList = $('.conversion-list')
+  let conversionHeader = $('.conversion')
+
+  if( conversionList.attr('id') === 'hide' ) {
+    conversionList.attr('id', 'show');
+    conversionHeader.text('- Conversion');
+  } else {
+    conversionList.attr('id', 'hide');
+    conversionHeader.text('+ Conversion');
+  }
+});
